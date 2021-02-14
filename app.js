d12e5b7dd5cf7fbfb9eb704a782876a35a18c521
marfgold1 <34912365+marfgold1@users.noreply.github.com>
2021-02-14 21:56:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+// Buatlah script agar .jam>p menampilkan jam client.
+// Format jam yang harus ditampilkan: JJ:MM:DD (J=jam, M=menit, D=detik)
+// DILARANG MENGGUNAKAN LIBRARY APAPUN, so, gunakanlah vanilla.js :)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Tugas Kadiv Web IMPACT 2021
+Buatlah sebuah web yang isinya menampilkan jam dunia (silahkan lihat file yang sudah diberikan).
